a4c5f003c5f38b7d98f1a487c17bd9a71e0245c3
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-17 12:34:19 +0200

    add snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,57 @@
+const cars = [
+  {
+    brand: "Opel",
+    model: "Corsa",
+    fuelType: "diesel",
+  },
+  {
+    brand: "BMW",
+    model: "Serie 1",
+    fuelType: "benzina",
+  },
+  {
+    brand: "Audi",
+    model: "A7",
+    fuelType: "benzina",
+  },
+  {
+    brand: "Opel",
+    model: "Corsa",
+    fuelType: "diesel",
+  },
+  {
+    brand: "Tesla",
+    model: "Model S",
+    fuelType: "elettrico",
+  },
+  {
+    brand: "Fiat",
+    model: "Panda",
+    fuelType: "metano",
+  },
+  {
+    brand: "Fiat",
+    model: "Punto",
+    fuelType: "gpl",
+  },
+  {
+    brand: "Mercedes",
+    model: "GLA",
+    fuelType: "benzina",
+  },
+  {
+    brand: "Ferrari",
+    model: "LaFerrari",
+    fuelType: "benzina",
+  },
+  {
+    brand: "Volkswagen",
+    model: "Touareg",
+    fuelType: "diesel",
+  },
+];
+
+const gasCars = cars.filter((car) => car.fuelType === "benzina");
+const dieselCars = cars.filter((car) => car.fuelType === "diesel");
+const otherCars = cars.filter((car) => car.fuelType !== "benzina" && car.fuelType !== "diesel");
+console.log(gasCars, dieselCars, otherCars);
